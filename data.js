02d1d7b89237a4418a061877a2c50346e92c2835
Alexis <62352242+ale-pe@users.noreply.github.com>
2020-12-16 18:17:17 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+repliquesbot =[
+    {
+        "id": "0",
+        "messagebot" : "Bonjour ! Je suis James ! Je suis là pour t'aider et te conseiller !",
+        "showoptions" : "text"
+    },
+    {
+        "id": "1",
+        "messagebot" : "Comment c'est passé ta journée ?",
+        "showoptions" : "buttons",
+        "options": ["Bien 😊","Moyen 😶",'Mal 😔'],
+        "gotooption" : [2,3,4]
+    }, {
+    "id": "2",
+    "messagebot" : "Super ! Content de savoir que ta journée c'est bien passée ! 😊",
+    "showoptions" : "buttons",
+    "options": ["Bien","Moyen",'Mal'],
+    "gotooption" : [2,3,4]
+}, 
+
+]
+
